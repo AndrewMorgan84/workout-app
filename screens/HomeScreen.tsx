47ee3d80b96711b0b07d2a69ce2b07237458ad30
Andrew <andrew.morgan@e.org>
2022-04-18 20:45:39 +0100

    added WorkoutItem and updated home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,18 +2,15 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import data from "../data.json";
 import { Workout } from "../types/data";
+import WorkoutItem from "../components/WorkoutItem";
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
-  const renderItem = ({ item }: { item: Workout }) => (
-    <View>
-      <Text>{`${item.name} - ${item.difficulty}`}</Text>
-    </View>
-  );
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>New Workouts</Text>
       <FlatList
         data={data as Workout[]}
-        renderItem={renderItem}
+        renderItem={WorkoutItem}
         keyExtractor={(item) => item.slug}
       />
     </View>
@@ -23,5 +20,11 @@ export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+    flex: 1,
+  },
+  header: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: "bold",
   },
 });
